e405e7c0cdd4ab6452c2c8c69e9e014f56afe0f1
artem <artemvasenko09@mail.ru>
2023-02-25 19:08:47 +0300

    added helic methods

--- Game/helicopter.py
+++ Game/helicopter.py
@@ -1,5 +1,5 @@
 from utils import randbool, randcell, randcell2
-
+from map import ASSETS
 class Helicopter:
     tank = 0 # вода
     mxtank = 1 # макс воды
@@ -11,4 +11,11 @@ class Helicopter:
         rc = randcell(self.h, self.w)
         rx, ry = rc[0], rc[1]
         self.x, self.y = rx, ry
-        # Походу полётов не будет...
+    def move(self, dx, dy):
+        nx, ny = dx + self.x, dy + self.y
+        if nx >= 0 and ny >= 0 and nx < self.h and ny < self.w:
+            self.x, self.y = nx, ny
+    
+    def print_stats(self):
+        print(ASSETS[9], self.tank, '/', self.mxtank, end='|')
+        print(ASSETS[10], self.score, end='|')
