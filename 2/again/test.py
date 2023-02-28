a3b16879a4d9f4fed328c0e5fc26fac8a7711e67
IsRonnin <103320407+IsRonnin@users.noreply.github.com>
2023-02-28 21:09:47 +0300

    Delete 2/again directory

--- 2/again/test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open(os.path.dirname(__file__) + "/requirements.txt", encoding="utf-16") as requirements_file:
-  if (os.path.isdir(os.path.dirname(__file__) + "/env/Lib/site-packages/pynput")
-  and "pynput==" in requirements_file.read()):
-    print("execution t")
-  else:
-    print("termination")
